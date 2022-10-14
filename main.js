c63b33404818a789824d2c98a578b6f80edc8f95
Ash <76790588+N0THSA@users.noreply.github.com>
2022-10-13 21:14:09 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+const database = [ // HELP
+	{
+		searchName: "Woohoo!",
+		searchDesc: "Woohoo! is an all in one search engine for the IntWeb, operated by the creators of the IntWeb.
+	},
+	{
+		searchName: "test3",
+		searchDesc: "Woohoo! is an all in one search engine for the IntWeb, operated by the creators of the IntWeb.
+	},
+	{
+		searchName: "test2",
+		searchDesc: "Woohoo! is an all in one search engine for the IntWeb, operated by the creators of the IntWeb.
+	},
+	{
+		searchName: "test1",
+		searchDesc: "Woohoo! is an all in one search engine for the IntWeb, operated by the creators of the IntWeb.
+	},
+	
+];
